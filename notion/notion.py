ec5fd723d21f198570b831366617be11f0dc098b
Tim Molderez <id@timmolderez.be>
2022-09-22 01:33:41 +0200

    QA tools - extra None check

--- notion/notion.py
+++ notion/notion.py
@@ -7,8 +7,8 @@ import mistletoe
 
 from notion.markdown import NotionBlockRenderer
 
-NOTION_API_URL = 'https://api.notion.com/v1/'
-NOTION_VERSION = '2021-05-13'
+NOTION_API_URL = "https://api.notion.com/v1/"
+NOTION_VERSION = "2021-05-13"
 PAGE_SIZE = 100
 
 
@@ -22,46 +22,58 @@ class NotionClient:
                 If this argument is not provided, the environment variable
                 NOTION_TOKEN is used instead.
         """
-        self.access_token = (access_token if access_token
-                             else getenv('NOTION_TOKEN'))
+        self.access_token = (
+            access_token if access_token else getenv("NOTION_TOKEN")
+        )
         if not self.access_token:
-            raise Exception("No Notion access token provided!\n"
-                            "Either set the NOTION_TOKEN environment "
-                            "variable or use the access_token argument.")
+            raise Exception(
+                "No Notion access token provided!\n"
+                "Either set the NOTION_TOKEN environment "
+                "variable or use the access_token argument."
+            )
 
     def get_database(self, database_id: str) -> Dict:
         """https://developers.notion.com/reference/get-database"""
-        return self._request(f'databases/{database_id}')
+        return self._request(f"databases/{database_id}")
 
-    def query_database(self, database_id: str,
-                       filter: Dict = {},
-                       sorts: List[Dict] = []) -> Generator[Dict, None, None]:
+    def query_database(
+        self, database_id: str, filter: Dict = {}, sorts: List[Dict] = []
+    ) -> Generator[Dict, None, None]:
         """https://developers.notion.com/reference/post-database-query"""
-        return self._paginated_request(f'databases/{database_id}/query',
-                                       data={'filter': filter, 'sorts': sorts})
+        return self._paginated_request(
+            f"databases/{database_id}/query",
+            data={"filter": filter, "sorts": sorts},
+        )
 
-    def create_database(self, parent: Dict,
-                        title: List[Dict],
-                        properties: Dict) -> Dict:
+    def create_database(
+        self, parent: Dict, title: List[Dict], properties: Dict
+    ) -> Dict:
         """https://developers.notion.com/reference/create-a-database"""
-        return self._request(f'databases',
-                             data={'parent': parent,
-                                   'title': title,
-                                   'properties': properties})
+        return self._request(
+            f"databases",
+            data={"parent": parent, "title": title, "properties": properties},
+        )
 
     def get_page(self, page_id: str) -> Dict:
         """https://developers.notion.com/reference/get-page"""
-        return self._request('pages/' + page_id)
+        return self._request("pages/" + page_id)
 
-    def create_page(self, parent: Dict,
-                    properties: Dict, children: List[Dict] = []) -> Dict:
+    def create_page(
+        self, parent: Dict, properties: Dict, children: List[Dict] = []
+    ) -> Dict:
         """https://developers.notion.com/reference/post-page"""
-        return self._request('pages', data={'parent': parent,
-                                            'properties': properties,
-                                            'children': children})
+        return self._request(
+            "pages",
+            data={
+                "parent": parent,
+                "properties": properties,
+                "children": children,
+            },
+        )
 
-    def create_page_markdown(self, parent: Dict,
-                             properties: Dict, md_body: IO):
+    def create_page_markdown(
+        self, parent: Dict, properties: Dict, md_body: IO
+    ):
         """
         Identical to `create_page`, but the page's body is provided as
         Markdown-formatted text.
@@ -70,28 +82,34 @@ class NotionClient:
             use `StringIO("my-markdown-string")` to convert it
             into a file-like object)
         """
-        children = mistletoe.markdown(md_body, NotionBlockRenderer)
+        children = []
+        if md_body is not None:
+            children = mistletoe.markdown(md_body, NotionBlockRenderer)
         return self.create_page(parent, properties, children)
 
-
-
-    def update_page(self, page_id: str, properties: Dict,
-                    archived: bool = False) -> Dict:
+    def update_page(
+        self, page_id: str, properties: Dict, archived: bool = False
+    ) -> Dict:
         """https://developers.notion.com/reference/patch-page"""
-        return self._request(f'pages/{page_id}',
-                             data={'properties': properties,
-                                   'archived': archived},
-                             req_method='PATCH')
+        return self._request(
+            f"pages/{page_id}",
+            data={"properties": properties, "archived": archived},
+            req_method="PATCH",
+        )
 
     def get_block_children(self, block_id: str) -> Generator[Dict, None, None]:
         """https://developers.notion.com/reference/get-block-children"""
-        return self._paginated_request(f'blocks/{block_id}/children')
+        return self._paginated_request(f"blocks/{block_id}/children")
 
-    def append_block_children(self, block_id: str,
-                              children: List[Dict]) -> Dict:
+    def append_block_children(
+        self, block_id: str, children: List[Dict]
+    ) -> Dict:
         """https://developers.notion.com/reference/patch-block-children"""
-        return self._request(f'blocks/{block_id}/children',
-                             data={'children':children}, req_method='PATCH')
+        return self._request(
+            f"blocks/{block_id}/children",
+            data={"children": children},
+            req_method="PATCH",
+        )
 
     def append_block_markdown(self, block_id: str, md_body: IO):
         """
@@ -105,30 +123,31 @@ class NotionClient:
         children = mistletoe.markdown(md_body, NotionBlockRenderer)
         return self.append_block_children(block_id, children)
 
-    def search(self, query: str,
-               sort: Dict = {}, filter: Dict = {}) -> Generator[Dict, None, None]:
+    def search(
+        self, query: str, sort: Dict = {}, filter: Dict = {}
+    ) -> Generator[Dict, None, None]:
         """https://developers.notion.com/reference/post-search"""
-        data = {'query': query}
+        data = {"query": query}
         if sort:
-            data['sort'] = sort
+            data["sort"] = sort
         if filter:
-            data['filter'] = filter
-        return self._paginated_request(
-            'search', data=data)
+            data["filter"] = filter
+        return self._paginated_request("search", data=data)
 
     def get_user(self, user_id: str) -> Dict:
         """https://developers.notion.com/reference/get-user"""
-        return self._request('users/' + user_id)
+        return self._request("users/" + user_id)
 
     def list_all_users(self) -> Generator[Dict, None, None]:
         """https://developers.notion.com/reference/get-users"""
-        return self._paginated_request('users')
+        return self._paginated_request("users")
 
-    def _paginated_request(self,
-                           url_path: str,
-                           data: Optional[Dict] = None,
-                           req_method: Optional[str] = None
-                           ) -> Generator[Dict, None, None]:
+    def _paginated_request(
+        self,
+        url_path: str,
+        data: Optional[Dict] = None,
+        req_method: Optional[str] = None,
+    ) -> Generator[Dict, None, None]:
         """
         Used for endpoints that return a paginated list of results.
         This function yields each result one by one, and will request the
@@ -140,7 +159,7 @@ class NotionClient:
             req_method: request method ('GET', 'POST' or 'PATCH')
                 (if not provided: 'POST' if data else 'GET')
         """
-        separator = '&' if '?' in url_path else '?'
+        separator = "&" if "?" in url_path else "?"
         next_cursor = None
         has_more = True
 
@@ -148,24 +167,28 @@ class NotionClient:
             paginated_url_path = url_path
             if data:
                 # Pass pagination parameters via POST/PATCH
-                data['page_size'] = PAGE_SIZE
+                data["page_size"] = PAGE_SIZE
                 if next_cursor:
-                    data['start_cursor'] = next_cursor
+                    data["start_cursor"] = next_cursor
             else:
                 # Pass pagination parameters via GET
-                params = f'page_size={PAGE_SIZE}'
+                params = f"page_size={PAGE_SIZE}"
                 if next_cursor:
-                    params += f'&start_cursor={next_cursor}'
+                    params += f"&start_cursor={next_cursor}"
                 paginated_url_path = url_path + separator + params
 
             response = self._request(paginated_url_path, data, req_method)
-            has_more = response.get('has_more', False)
-            next_cursor = response.get('next_cursor', None)
-            for result in response['results']:
+            has_more = response.get("has_more", False)
+            next_cursor = response.get("next_cursor", None)
+            for result in response["results"]:
                 yield result
 
-    def _request(self, url_path: str, data: Dict = None,
-                 req_method: Optional[str] = None) -> Dict:
+    def _request(
+        self,
+        url_path: str,
+        data: Dict = None,
+        req_method: Optional[str] = None,
+    ) -> Dict:
         """
         Make an HTTP request to the given URL
         Args:
@@ -178,15 +201,16 @@ class NotionClient:
         """
         data_bytes = None
         if data:
-            data_bytes = str(json.dumps(data)).encode('utf-8')
-        req = Request(NOTION_API_URL + url_path, data=data_bytes,
-                      method=req_method)
+            data_bytes = str(json.dumps(data)).encode("utf-8")
+        req = Request(
+            NOTION_API_URL + url_path, data=data_bytes, method=req_method
+        )
         # The default urllib user agent is blocked, so let's use something else
-        req.add_header('User-Agent', "curl/7.70.0")
+        req.add_header("User-Agent", "curl/7.70.0")
         req.add_header("Authorization", "Bearer " + self.access_token)
         req.add_header("Notion-Version", NOTION_VERSION)
         if data:
-            req.add_header('Content-Type', 'application/json')
+            req.add_header("Content-Type", "application/json")
 
         response = urlopen(req)
-        return json.loads(response.read().decode('utf-8'))
+        return json.loads(response.read().decode("utf-8"))
